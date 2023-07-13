b3e013fa6d035627bf44bc2118c51d7103c05b73
peter6070 <peter6070@naver.com>
2023-07-13 23:34:53 +0900

    update site
    사이트 제작중

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
-function Alert(){
-    alert("I am alert!");
+function Alert() {
+  alert("I am alert!");
 }
 
-const menu = document.querySelectorAll("li");
+const $topBtn = document.querySelector(".moveTopBtn");
+
+// 버튼 클릭 시 맨 위로 이동
+$topBtn.onclick = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+const $bottomBtn = document.querySelector(".moveBottomBtn");
+
+// 버튼 클릭 시 페이지 하단으로 이동
+$bottomBtn.onclick = () => {
+  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+};
