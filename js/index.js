5c7442a99a6c2a1f56b2adaa8ec9d177adaec7e1
peter6070 <peter6070@naver.com>
2023-07-14 01:55:25 +0900

    update project
    프로젝트 진행중

--- js/index.js
+++ js/index.js
@@ -6,13 +6,24 @@ const $topBtn = document.querySelector(".moveTopBtn");
 // 버튼 클릭 시 맨 위로 이동
 $topBtn.onclick = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
+  page = 1;
 };
 
 const $bottomBtn = document.querySelector(".moveBottomBtn");
 // 버튼 클릭 시 페이지 하단으로 이동
 $bottomBtn.onclick = () => {
   window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  page = lastPage;
 };
+
+const $li = document.querySelectorAll("#side-menu li");
+
+$li.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    page = index + 1;
+    console.log(page);
+  });
+});
 
 // 스크롤 시 한 영역씩 이동하는 코드
 window.addEventListener(
@@ -27,7 +38,7 @@ window.addEventListener(
 //> true인 경우 : 스크롤이벤트를 막지 않겠다! preventDefault()를 사용할 수 없다.
 
 var $html = $("html");
-var page = 1; //뷰포트에 표시되는 페이지 번호
+var page = 0; //뷰포트에 표시되는 페이지 번호
 var lastPage = $(".content").length; //마지막 페이지 번호(전체 content 클래스의 길이 = 개수)
 $html.animate({ scrollTop: 0 }, 10); //문서 로드되면 첫 페이지로
 
@@ -41,13 +52,20 @@ $(window).on("wheel", function (e) {
     //e(jQuery 반환)
     //.originalEvent(js에서의 원래 이벤트)
     //.deltaY(마우스 휠을 어느 방향으로 얼만큼 굴렸는지)
-    if (page == lastPage) return; //마지막 페이지면 이벤트 핸들러 종료(스크롤 될것이 없어서)
+    if (page == lastPage + 1) {
+      //첫 번째 페이지면 이벤트 핸들러 종료(스크롤 될것이 없어서)
+      return;
+    }
     page++; //아니면 페이지 1 증가
   } else if (e.originalEvent.deltaY < 0) {
     //마우스 휠을 위쪽으로 굴렸을때
-    if (page == 1) return; //첫 번째 페이지면 이벤트 핸들러 종료
-    page--; //아니면 페이지 1 감소
+    if (page == 0) {
+      //첫 번째 페이지면 이벤트 핸들러 종료
+      return;
+    } else page--; //아니면 페이지 1 감소
   }
-  var posTop = (page - 1) * $(window).height(); //이동할 페이지 번호 스크롤 위치 계산
-  $html.animate({ scrollTop: posTop }); //계산한 위치로 이동
+  var posTop =
+    (page - 1) * $(window).height() + $("header").height() + 75 + page * page; //이동할 페이지 번호 스크롤 위치 계산
+  if (page == 0) $html.animate({ scrollTop: 0 }); //계산한 위치로 이동
+  else $html.animate({ scrollTop: posTop }); //계산한 위치로 이동
 });
